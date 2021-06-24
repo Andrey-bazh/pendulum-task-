c1b378c398a0833b2fe6e68a982fa010d3bc3e2f
Andrey B <bazh.andrey8@gmail.com>
2021-06-24 19:18:26 +0300

    t1 task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Sketch from "react-p5";
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [length, setLength] = useState(350)
+
+    const addLength = () => {
+        setLength(prevState => +20)
+    }
+    const removeLength = () => {
+    setLength(prevState => -20)
+    }
+    let angle; // угол мятника
+    let velocity = 0; // скорость
+    let acceleration = 0; // ускорение
+    let bob; // гиря
+    // let length; // длинна веревки на которой держится гиря
+    let origin; // исходное положение
+    let gravity = 1; // силя тяжести
+
+    let setup = (p5, canvasParentRef) => {
+        p5.createCanvas(700, 500).parent(canvasParentRef);
+        p5.frameRate(React.Component.frameRate);
+        // length = 300;  // зададим значения переменным
+        angle = Math.PI / 2;
+        bob = p5.createVector();
+        origin = p5.createVector(300, 0);
+    };
+
+    let draw = (p5) => {
+        p5.background(207, 255, 233);
+
+        let force = gravity * Math.sin(angle);
+        acceleration = (-1 * force) / length;
+        velocity += acceleration;
+        angle += velocity;
+
+        bob.x = length * Math.sin(angle) + origin.x;
+        bob.y = length * Math.cos(angle) + origin.y;
+
+        p5.stroke(207, 222, 255); // цвет веревки
+        p5.strokeWeight(11); // толщина веревки
+
+        p5.fill(255, 81, 142); // цвет гири
+
+        p5.line(origin.x, origin.y, bob.x, bob.y);
+        p5.circle(bob.x, bob.y, 64);
+    };
+
+
+    return (
+        <div className="App">
+            <Sketch setup={setup} draw={draw} className="App"/>
+            <button onClick={() => addLength()}>Увеличить веревку</button>
+            <button onClick={() => removeLength()}>Уменьшить веревку</button>
+        </div>
+    );
 }
 
 export default App;
