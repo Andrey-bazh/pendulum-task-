a48e37074679c55d7fcac2caf597caccf8c64319
Andrey B <bazh.andrey8@gmail.com>
2021-07-06 16:07:18 +0300

    fix bags

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, {useState} from 'react'
 import Sketch from "react-p5";
 import './app.css'
 
-function App() {
-    const [length, setLength] = useState(350)
+function App() { 
+    let length = 350; // длинна веревки
+    let bobWeight = 1; // вес гири
 
     const addLength = () => {
-        setLength(prevState => +20)
+        length += 10;
     }
     const removeLength = () => {
-    setLength(prevState => -20)
+        if(length === 10) return;
+        length -= 10;
+    }
+
+    const addWeight = () => {
+        if(bobWeight === 1) return;
+        bobWeight -= 1;
+    }
+
+    const removeWeight = () =>{
+        bobWeight += 1;
     }
     let angle; // угол мятника
     let velocity = 0; // скорость
     let acceleration = 0; // ускорение
+    // let bobWeight; //вес гири
     let bob; // гиря
     // let length; // длинна веревки на которой держится гиря
     let origin; // исходное положение
@@ -31,29 +43,32 @@ function App() {
     let draw = (p5) => {
         p5.background(207, 255, 233);
 
-        let force = gravity * Math.sin(angle);
+        let force = gravity * Math.sin(angle) * bobWeight;
         acceleration = (-1 * force) / length;
         velocity += acceleration;
         angle += velocity;
-
         bob.x = length * Math.sin(angle) + origin.x;
         bob.y = length * Math.cos(angle) + origin.y;
 
         p5.stroke(207, 222, 255); // цвет веревки
-        p5.strokeWeight(11); // толщина веревки
+        p5.strokeWeight(10); // толщина веревки
 
         p5.fill(255, 81, 142); // цвет гири
 
         p5.line(origin.x, origin.y, bob.x, bob.y);
-        p5.circle(bob.x, bob.y, 64);
+        p5.circle(bob.x, bob.y, 65);
     };
 
 
     return (
         <div className="App">
             <Sketch setup={setup} draw={draw} className="App"/>
+            <div className='buttons-wrapper'>
             <button onClick={() => addLength()}>Увеличить веревку</button>
             <button onClick={() => removeLength()}>Уменьшить веревку</button>
+            <button onClick={() => addWeight()}>Увеличить груз</button>
+            <button onClick={() => removeWeight()}>Уменьшить груз</button>
+            </div>
         </div>
     );
 }
